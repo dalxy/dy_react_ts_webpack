5ff750ca34fb8b8436864a14ea73b597036d0f5c
dalaxy <z123xcv@foxmail.com>
2023-03-09 15:44:04 +0800

    fix: 添加icon

--- src/components/SiderBar.tsx
+++ src/components/SiderBar.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 // import styled from "styled-components";
-// import { Breadcrumb, Layout, Menu, theme } from "antd";
-import { Layout, Space } from "antd";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
 import {
-    HomeOutlined,
     DesktopOutlined,
     FileOutlined,
     PieChartOutlined,
@@ -31,24 +29,45 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-    // getItem("Option 1", "1"),
-    getItem("Option 1", "1", <HomeOutlined />),
-    // getItem("Option 2", "2", <DesktopOutlined />),
-    // getItem("User", "sub1", <UserOutlined />, [
-    //     getItem("Tom", "3"),
-    //     getItem("Bill", "4"),
-    //     getItem("Alex", "5"),
-    // ]),
-    // getItem("Team", "sub2", <TeamOutlined />, [
-    //     getItem("Team 1", "6"),
-    //     getItem("Team 2", "8"),
-    // ]),
-    // getItem("Files", "9", <FileOutlined />),
+    getItem("Option 1", "1", <PieChartOutlined />),
+    getItem("Option 2", "2", <DesktopOutlined />),
+    getItem("User", "sub1", <UserOutlined />, [
+        getItem("Tom", "3"),
+        getItem("Bill", "4"),
+        getItem("Alex", "5"),
+    ]),
+    getItem("Team", "sub2", <TeamOutlined />, [
+        getItem("Team 1", "6"),
+        getItem("Team 2", "8"),
+    ]),
+    getItem("Files", "9", <FileOutlined />),
 ];
 
-const SiderBar = () => {
-    // <HomeOutlined />
-    return;
+const SiderBar: React.FC = () => {
+    const [ collapsed, setCollapsed ] = useState(false)
+    
+    const { token: { colorBgContainer } } = theme.useToken()
+    return (
+        <Layout style={{minHeight:'100vh'}}>
+            <Sider collapsed={collapsed} collapsible onCollapse={(value)=>setCollapsed(value)}>
+                <div style={{height: 32, margin: 16, background: 'rgba(255,255,255,.2)'}} />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+            </Sider>
+            <Layout className="site-layout">
+                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Content style={{ margin: '0 16px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb items={[{title: 'sample'}]}>User</Breadcrumb>
+                    <Breadcrumb items={[{title: 'sample'}]}>Bill</Breadcrumb>
+                </Breadcrumb>
+                <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+                    Bill is a cat.
+                </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+            </Layout>
+        </Layout>
+    )
 };
 
 export default SiderBar;
